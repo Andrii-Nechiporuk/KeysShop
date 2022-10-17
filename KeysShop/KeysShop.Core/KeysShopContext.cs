a902e7d67c7025ec52d66dbe9e2f4328355bd4cc
Yurii-Polischukkk <113755012+Yurii-Polischukkk@users.noreply.github.com>
2022-10-17 20:22:40 +0300

    Common_user_edited
    +web app fix♥

---
+++ KeysShop/KeysShop.Core/KeysShopContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KeysShop.Core
+{
+    public class KeysShopContext : IdentityDbContext<User>
+    {
+        public KeysShopContext(DbContextOptions<KeysShopContext> options)
+            : base(options)
+        {
+        }
+    }
+}
